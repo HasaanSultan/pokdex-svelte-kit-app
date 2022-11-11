8c25f931c2b90f945a2721662d7f8dd8b06a9083
Hasaan Sultan <Hasaan.sultan@visionx.io>
2022-11-11 14:27:45 +0500

    added pokemon detail page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,19 @@
 <script>
 	import { pokemon } from '../stores/pokestore';
 	import PokemanCard from '../components/pokemanCard.svelte';
-	console.log($pokemon);
+
+	let searchTerm = '';
+	let filterePokemon = [];
+
+	$: {
+		if (searchTerm) {
+			filterePokemon = $pokemon.filter((pokeman) =>
+				pokeman.name.toLowerCase().includes(searchTerm.toLowerCase())
+			);
+		} else {
+			filterePokemon = [...$pokemon];
+		}
+	}
 </script>
 
 <svelte:head>
@@ -9,9 +21,18 @@
 </svelte:head>
 
 <h1 class="text-4xl text-center my-8 uppercase">Svelte Kit Pokedex</h1>
-{#each $pokemon as pokeman}
-	<PokemanCard {pokeman} />
-{/each}
+
+<input
+	type="text"
+	class="w-full round-md text-lg p-4 border-2 border-gray-200"
+	placeholder="Search Pokemon"
+	bind:value={searchTerm}
+/>
+<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
+	{#each filterePokemon as pokeman}
+		<PokemanCard {pokeman} />
+	{/each}
+</div>
 
 <style>
 	h1 {
